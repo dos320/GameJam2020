a450a56612195b695e8a2f121907060754119346
Howard Zhang <how613@hotmail.com>
2020-02-01 10:46:38 -0500

    test

---
+++ GameJam2020/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 movement;
+    private float horizontal;
+    private float vertical;
+    public float movement_speed;
+
+    void Start()
+    {
+        movement = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+        movement = new Vector3(horizontal * movement_speed, vertical * movement_speed, 0);
+        transform.Translate(movement * Time.deltaTime);
+        
+    }
+}
---
+++ GameJam2020/Assets/scripts/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool pickUp;
+    void Start()
+    {
+        pickUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            pickUp = true;
+        }
+        else
+        {
+            pickUp = false;
+        }
+
+        if (pickUp)
+        {
+
+        }   
+    }
+}
